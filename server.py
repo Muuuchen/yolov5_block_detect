6a43e279b86f3d0b86e6c603180bcb8978aea7ca
燕支雪 <9984853+yan-zhixue@user.noreply.gitee.com>
2022-07-01 02:12:41 +0000

    update server.py.

--- server.py
+++ server.py
@@ -4,34 +4,56 @@ from socket import *
 import sys
 import re
 
-ipDefault = '192.168.10.211'
-port = 8080
+ipDefault = '192.168.43.25'
+port = 8083
 
-def setAddr(ip = '0.0.0.0'):
-    if ip == '0.0.0.0':
-        try:
-            addr = (sys.argv[1], port)
-            print("Input ip {} suceess.".format(addr[0]))
-        except:
-            addr = (ipDefault, port)
-            print("ipDefault {}.".format(addr[0]))
-    else:
-        addr = (ip, port)
-        print("Set ip {} suceess.".format(addr[0]))
-    assert bool(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", addr[0]))
-    return addr
+class server():
+    def __init__(self):
+        self.sss = socket(AF_INET, SOCK_DGRAM)
+        self.setAddr()
+        self.sss.bind(self.addr)
+        print("---------------------if close needed, Ctrl+C please---------------------")
 
-s = socket(AF_INET, SOCK_DGRAM)
-s.bind(setAddr())
-while True:
-    data, _ = s.recvfrom(921600)
-    receive_data = np.frombuffer(data, dtype='uint8')
-    r_img = cv2.imdecode(receive_data, 1)
-    r_img = r_img.reshape(480, 640, 3)
+    def __del__(self):
+        '''
+        !-- only work at the period of close normally or Ctrl+C
+        and then port would be released
+        :return:
+        '''
+        self.sss.close()
 
-    # cv2.putText(r_img, "server", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-    cv2.imshow('server_frame', r_img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    def setAddr(self, ip = '0.0.0.0'):
+        if ip == '0.0.0.0':
+            try:
+                addr = (sys.argv[1], port)
+                print("Input ip {} suceess.".format(addr[0]))
+                assert bool(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", addr[0]))
+            except:
+                addr = (ipDefault, port)
+                print("ipDefault {}.".format(addr[0]))
+        else:
+            addr = (ip, port)
+            print("Set ip {} suceess.".format(addr[0]))
+        assert bool(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", addr[0]))
+        self.addr = addr
 
-cv2.destroyAllWindows()
+    def Recv(self):
+        while True:
+            data, _ = self.sss.recvfrom(921600)
+            receive_data = np.frombuffer(data, dtype='uint8')
+            r_img = cv2.imdecode(receive_data, 1)
+            r_img = r_img.reshape(480, 640, 3)
+
+            # cv2.putText(r_img, "server", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            cv2.imshow('server_frame', r_img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    obj = server()
+    obj.Recv()
+
