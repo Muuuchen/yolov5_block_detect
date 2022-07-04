e7610484c634f7a1e53c94a2834fec2b2336da23
燕支雪 <9984853+yan-zhixue@user.noreply.gitee.com>
2022-07-04 14:39:55 +0000

    update ClosestBlock1.py.

--- ClosestBlock1.py
+++ ClosestBlock1.py
@@ -1,17 +1,48 @@
 import cv2
 import numpy as np
-
-imshowFlag = True
+from init import imshowFlag
 
 def adaptx(x):
     return max(min(x,639),0)
 def adapty(x):
     return max(min(x,479),0)
 
-def myShow(Colormask=None, imdep1=None, mask=None):
+# Histogram equalization
+def histequ(gray, nlevels=256):
+    # Compute histogram
+    histogram = np.bincount(gray.flatten(), minlength=nlevels)
+    # print ("histogram: ", histogram)
+
+    # Mapping function
+    uniform_hist = (nlevels - 1) * (np.cumsum(histogram)/(gray.size * 1.0))
+    uniform_hist = uniform_hist.astype('uint8')
+    # print ("uniform hist: ", uniform_hist)
+
+    # Set the intensity of the pixel in the raw gray to its corresponding new intensity
+    height, width = gray.shape
+    uniform_gray = np.zeros(gray.shape, dtype='uint8')  # Note the type of elements
+    for i in range(height):
+        for j in range(width):
+            uniform_gray[i,j] = uniform_hist[gray[i,j]]
+
+    return uniform_gray
+
+def myShow(imgdep=None, Colormask=None, imdep1=None, dilation=None, img=None):
+    if imshowFlag and not (imgdep == None).any(): cv2.imshow("imgdep", histequ(imgdep))
     if imshowFlag and not (Colormask == None).any(): cv2.imshow("Color Mask", Colormask)
     if imshowFlag and not (imdep1 == None).any(): cv2.imshow("depmask",imdep1)
-    if imshowFlag and not (mask == None).any(): cv2.imshow("mask", mask)
+    if imshowFlag and not (dilation == None).any(): cv2.imshow("dilation", dilation)
+    if imshowFlag and not (img == None).any(): cv2.imshow("imgCB", img)
+
+def MSE(tensor):
+    arr = np.array(tensor)
+
+    num = 1
+    shape = np.shape(arr)
+    for i in range(len(shape)): num *= shape[i]
+    averArr = np.ones(shape) * np.average(arr)
+
+    return np.linalg.norm(arr - averArr) / num
 
 def Closest_Block(imgdep, img, getpoint,colorflag):
     '''
@@ -19,11 +50,11 @@ def Closest_Block(imgdep, img, getpoint,colorflag):
     :param imgdep:
     :param img:
     :param getpoint: center in white
-    :param colorflag:
+    :param colorflag: Red 0 Blue 1
     :return:    sum1: depth
                 flag_temp: if 0, continue in the loop
                 cX, cY: the center of the white block
-                isside_temp
+                isside_temp: -1 means side lie down, 1 menas side stand
     '''
     l = adaptx(int(getpoint[0][0]))
     u = adapty(int(getpoint[0][1]))
@@ -32,7 +63,6 @@ def Closest_Block(imgdep, img, getpoint,colorflag):
     if ((r-l)*(d-u)<=30*30):
         return float("inf"), 0, (l+r)/2,(u+d)/2, 0
 
-    # colorflag: Red 0 blue 1
     # RB = np.array([[[129, 57, 84], [179, 255, 255]], [[48, 43, 0], [144, 255, 255]]], dtype=np.uint8)
     R1 = np.array([[0, 68, 59], [20, 255, 255]], dtype=np.uint8)
     R2 = np.array([[150, 68, 59], [179, 255, 255]], dtype=np.uint8)
@@ -47,7 +77,7 @@ def Closest_Block(imgdep, img, getpoint,colorflag):
     x1 = int((l + r)/2)
     y1 = int((u + d)/2)
     p = int(imgdep[y1][x1])
-    imdep1 = cv2.inRange(imgdep,max(p-150,1),p+150)
+    imdep1 = cv2.inRange(imgdep,max(p-500,1),p+500)
     imgdep_roi = cv2.fillConvexPoly(np.zeros(imgdep.shape,np.uint8), Rect, (255))
     imdep1 = cv2.bitwise_and(imdep1,imgdep_roi)
     imdep1 = cv2.erode(imdep1, (3, 3), iterations=1)
@@ -56,7 +86,7 @@ def Closest_Block(imgdep, img, getpoint,colorflag):
 
     Colormask = cv2.bitwise_or(cv2.inRange(imghsv, R1[0], R1[1]), cv2.inRange(imghsv, R2[0], R2[1])) \
         if not colorflag else cv2.inRange(imghsv, B[0], B[1])
-    mask = cv2.bitwise_xor(Colormask,imdep1)
+    mask = cv2.bitwise_xor(Colormask, imdep1)
     erosion = cv2.erode(mask, (3, 3), iterations=1)
     dilation = cv2.dilate(erosion, (3, 3), iterations=1)
     mask = dilation
@@ -77,8 +107,8 @@ def Closest_Block(imgdep, img, getpoint,colorflag):
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) == 0: return float("inf"),0,cX,cY,1
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-    myShow(Colormask=Colormask, imdep1=imdep1, mask=img)
+    cv2.drawContours(img, contours, 0, (0, 255, 0), 3)
+    myShow(imgdep=imgdep, Colormask=Colormask, imdep1=imdep1, dilation=dilation, img=img)
 
     cont = contours[0]
     if (cv2.contourArea(cont) * 8 > (r-l) * (d-u)):
@@ -91,6 +121,12 @@ def Closest_Block(imgdep, img, getpoint,colorflag):
         else:
             return float("inf"),0,cX,cY,0
     elif imgdep[y1][x1]!=0:
-        return imgdep[y1][x1],1,cX,cY,1
+        stamp = 20
+        tensor1 = [imgdep[adapty(y1 + i * stamp)][x1] for i in range(-5, 6, 1)]
+        tensor2 = [imgdep[y1][adaptx(x1 + i * stamp)] for i in range(-5, 6, 1)]
+        print(tensor1)
+        print(tensor2)
+        if MSE(tensor1) > MSE(tensor2): return imgdep[y1][x1],1,cX,cY,1
+        else: return imgdep[y1][x1], 1, cX, cY, -1
     else:
         return float("inf"),0,cX,cY,1
