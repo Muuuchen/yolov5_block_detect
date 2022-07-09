1d1d64309d935d85db96d15f01a534bd0d184cd7
燕支雪 <9984853+yan-zhixue@user.noreply.gitee.com>
2022-07-09 03:12:10 +0000

    update init.py.

--- init.py
+++ init.py
@@ -3,12 +3,3 @@ ipDefault = '192.168.10.211'
 portDefault = 8080
 ipclient = '192.168.10.197'
 R2 = False
-
-
-
-
-cameras = ['0', '8'] if R2 else ['0']
-with open('./forTest/stream.txt', 'w') as f:
-    for camera in cameras:
-        f.write(camera)
-        if not cameras.index(camera) + 1 == len(cameras): f.write('\n')
